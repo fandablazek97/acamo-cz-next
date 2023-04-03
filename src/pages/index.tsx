f64768284fade1c6572a6cb6f0feb335df981e8b
fandablazek <82412830+fandablazek97@users.noreply.github.com>
2023-04-03 11:52:20 +0200

    oprava překlepu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,18 +8,23 @@ export default function Home() {
     <>
       <Seo description="ACAMO DIGITAL is a design agency with 5+ years of experience crafting pixel-perfect designs for clients. Our specialties include UI/UX, app and website development, and brand strategy. We deliver high-quality, custom solutions tailored to our clients' unique needs. Our clients include Pilsner Urquell, Skoda Transportation, and more. Get in touch with us at hello@acamo.cz or +420 775 210 897" />
       <header className="pb-6">
-      <Heading level={1} hasMarginBottom>
-        ACAMO DIGITAL
-      </Heading>
-      <Paragraph>We&apos;re so busy crafting pixel-perfect designs for our clients that our own website has become the digital equivalent of a sticky note!</Paragraph>
+        <Heading level={1} hasMarginBottom>
+          ACAMO DIGITAL
+        </Heading>
+        <Paragraph>
+          We&apos;re so busy crafting pixel-perfect designs for our clients that
+          our own website has become the digital equivalent of a sticky note!
+        </Paragraph>
       </header>
 
       <section className="py-6">
         <Heading level={2} hasMarginBottom>
-        What we do
+          What we do
         </Heading>
         <Paragraph>
-        With over 5 years of experience in the industry, we have a proven track record of delivering high-quality, custom solutions that meet the unique needs of our clients.
+          With over 5 years of experience in the industry, we have a proven
+          track record of delivering high-quality, custom solutions that meet
+          the unique needs of our clients.
         </Paragraph>
         <List className="mt-4">
           <List.Item>UI & UX;</List.Item>
@@ -33,7 +38,9 @@ export default function Home() {
           Who did we work for?
         </Heading>
         <Paragraph>
-        F.H. Prager, Strava.cz, Pilsner Urquell, Bestaudio.cz, Skoda Transportation, Prager&apos;s, Nadační fond muži proti rakovině, Charles University, Jack Daniel&apos;s and much more…
+          F.H. Prager, Strava.cz, Pilsner Urquell, Bestaudio.cz, Skoda
+          Transportation, Prager&apos;s, Nadační fond muži proti rakovině,
+          Charles University, Jack Daniel&apos;s and much more…
         </Paragraph>
       </section>
 
@@ -42,13 +49,26 @@ export default function Home() {
           How to contact us
         </Heading>
         <Paragraph>
-          Say hello to as at <a href="mailto:hello@acamo.cz" className="underline hover:bg-white hover:text-zinc-900 hover:no-underline">hello@acamo.cz</a> or call: <a href="tel:+420775210897" className="underline hover:bg-white hover:text-zinc-900 hover:no-underline">+420 775 210 897</a>
+          Say hello to us at{" "}
+          <a
+            href="mailto:hello@acamo.cz"
+            className="underline hover:bg-white hover:text-zinc-900 hover:no-underline"
+          >
+            hello@acamo.cz
+          </a>{" "}
+          or call:{" "}
+          <a
+            href="tel:+420775210897"
+            className="underline hover:bg-white hover:text-zinc-900 hover:no-underline"
+          >
+            +420 775 210 897
+          </a>
         </Paragraph>
       </section>
 
       <section className="py-6">
         <Heading level={2} hasMarginBottom color="red">
-        Who are we hiring?
+          Who are we hiring?
         </Heading>
         <List className="mt-4">
           <List.Item>UI & UX designer;</List.Item>
@@ -60,11 +80,12 @@ export default function Home() {
         <Heading level={2} color="muted" hasMarginBottom>
           Company details
         </Heading>
-        <Paragraph className="text-zinc-500 text-sm">
+        <Paragraph className="text-sm text-zinc-500">
           Company: AKLD Group s.r.o. <br />
           Residence: Dílenská 172/18, Doubravka, 312 00 Plzeň <br />
           Identification Number: 08371415 <br />
-          The company is registered in the Commercial Register kept at the Regional Court in Pilsen, Section C, Insert 3108
+          The company is registered in the Commercial Register kept at the
+          Regional Court in Pilsen, Section C, Insert 3108
         </Paragraph>
       </section>
     </>
